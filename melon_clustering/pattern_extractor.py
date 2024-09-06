8686c88255658e605c28af1e8781f8926e7a47aa
1a2a34v <1a2a3a4v@gmail.com>
2024-09-07 00:23:40 +0200

    working?

--- melon_clustering/pattern_extractor.py
+++ melon_clustering/pattern_extractor.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.mixture import GaussianMixture
 from sklearn.manifold import MDS
 from sklearn.metrics import silhouette_score
-from sklearn.cluster import KMeans, DBSCAN, AgglomerativeClustering
 from collections import OrderedDict
 from typing import List, Tuple, Dict
+import re
 
 class PatternExtractor:
     def __init__(self, language: str, radius: int = 5, filter_threshold: int = 3):
@@ -30,6 +31,7 @@ class PatternExtractor:
             if pattern not in pattern_dict:
                 pattern_dict[pattern] = 0
             pattern_dict[pattern] += 1
+        print("pattern_dict",pattern_dict)
         return pattern_dict
 
     def get_token_list(self, sentences: List[str]) -> List[List[str]]:
@@ -37,13 +39,24 @@ class PatternExtractor:
             return [['<START>'] + list(sentence.lower()) + ['<END>'] for sentence in sentences]
         return [['<START>'] + sentence.lower().split() + ['<END>'] for sentence in sentences]
 
+    def morphology_in_token(self, morphology, token):
+        if morphology.lower() in token.lower():
+            trailing_part = re.sub(re.escape(morphology.lower()), '', token.lower(), count=1)
+            print("hah",trailing_part)
+            if not bool(re.search(r'[a-z]', trailing_part)):
+                print("TRH")
+                return True
+        return False
+
     def extract_patterns_flat(self, sentences_by_morphology: Dict[str, List[str]]) -> Dict:
         pattern_dict = {}
+
         for morphology, sentences in sentences_by_morphology.items():
             for tokens in self.get_token_list(sentences):
                 for idx, token in enumerate(tokens):
-                    if token == morphology.lower():
+                    if self.morphology_in_token(morphology, token):
                         pattern_dict = self._build_pattern_flat(tokens, idx, pattern_dict)
+
         filtered_pattern_dict = OrderedDict(
             {pattern: count for pattern, count in sorted(pattern_dict.items(), key=lambda x: x[1], reverse=True)
              if count >= self.filter_threshold}
@@ -53,22 +66,27 @@ class PatternExtractor:
     def map_sentences_to_patterns(self, sentences_by_morphology: Dict[str, List[str]]) -> List[Tuple[str, List[int]]]:
         sentence_pattern_mapping = []
         self.pattern_list = list(self.mask_patterns.keys())
+
         for morphology, sentences in sentences_by_morphology.items():
             for sentence in self.get_token_list(sentences):
                 tokens = ['<START>'] + [token.lower() for token in sentence] + ['END']
                 matched_indices = set()
                 for idx, token in enumerate(tokens):
-                    if token == morphology.lower():
+                    if self.morphology_in_token(morphology, token):
                         window_start = max(0, idx - self.radius)
                         window_end = min(len(tokens), idx + self.radius + 1)
                         window = tokens[window_start:window_end]
                         window[idx - window_start] = '<MASK>'
+                        print("window",window)
                         if ('<MASK>',) in self.pattern_list:
                             matched_indices.add(self.pattern_list.index(('<MASK>',)))
+
                         for i, pattern in enumerate(self.pattern_list):
                             pattern_length = len(pattern)
                             for j in range(len(window) - pattern_length + 1):
                                 if tuple(window[j:j + pattern_length]) == pattern:
                                     matched_indices.add(i)
+
                 sentence_pattern_mapping.append((sentence, sorted(matched_indices)))
+
         return sentence_pattern_mapping
