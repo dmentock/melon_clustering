07259ce091acf21f7392dcb0ba439c1a4329ce0d
1a2a34v <1a2a3a4v@gmail.com>
2024-09-10 21:21:29 +0200

    merge

--- melon_clustering/__init__.py
+++ melon_clustering/__init__.py
@@ -3,5 +3,5 @@ PROJECT_ROOT = Path(__file__).parent.parent
 SENTENCES_DIR = PROJECT_ROOT / '_sentences'
 CACHE_DIR = PROJECT_ROOT / '_cache'
 
-from .clustering import Clustering
+# from .clustering import Clustering
 from .pattern_extractor import PatternExtractor
